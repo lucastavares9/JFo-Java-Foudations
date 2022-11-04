e729dc1bf202db90379b4648d65b988edcd3fe52
Lucas Tavares <lucasdtavares@gmail.com>
2022-11-04 16:34:26 -0300

    commited

---
+++ src/ArteASCIISimples.java
@@ -0,0 +1,12 @@
+public class ArteASCIISimples{
+    public static void main(String[] args) {
+        System.out.println("  XXXX  ");
+        System.out.println(" X    X ");
+        System.out.println("X X  X X");
+        System.out.println("X      X");
+        System.out.println("X X  X X");
+        System.out.println("X  XX  X");
+        System.out.println("  XXXX  ");
+        
+    }
+}
---
+++ src/ArteASCIISimples2.java
@@ -0,0 +1,12 @@
+public class ArteASCIISimples2 {
+    public static void main(String[] args) {
+        System.out.println("  XXXX  ");
+        System.out.println(" X    X ");
+        System.out.println("X X  X X");
+        System.out.println("X      X");
+        System.out.println("X  XX  X");
+        System.out.println("X X  X X");
+        System.out.println(" X    X ");
+        System.out.println("  XXXX  ");
+    }
+}
