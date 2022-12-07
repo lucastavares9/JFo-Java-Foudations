42fd3a6e36448e3fcff760a1ca27160601fae748
Lucas Tavares <lucasdtavares@gmail.com>
2022-12-06 21:01:08 -0300

    commit corrections

--- src/ProcessName.java
+++ src/ProcessName.java
@@ -2,18 +2,12 @@ import java.util.Scanner;
 
 public class ProcessName {
     public static void main(String[] args) {
-        System.out.println("Bem vindo ao progama Process Name!");
-        System.out.println();
+        System.out.println("Bem vindo ao progama Process Name !");
         Scanner sc = new Scanner(System.in);
-        System.out.print("Insira seu nome:");
-        String nome1 = sc.nextLine();
-
-        String[] nomeseparado = nome1.split("");
-
-        System.out.println(nomeseparado[(nomeseparado.length - 1)]+ "" +nomeseparado[0].substring(0,1));
-       
-        
+        System.out.print("Qual o seu nome:  ");
+        String nome = sc.nextLine();
+        String[] nomeSeparado = nome.split(" ");
+        System.out.println(nomeSeparado[(nomeSeparado.length - 1)] + " " + nomeSeparado[0].substring(0,1));
         sc.close();
-
     }
 }
